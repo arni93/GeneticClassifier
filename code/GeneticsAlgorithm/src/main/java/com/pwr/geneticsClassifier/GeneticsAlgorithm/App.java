e9e68156a7eccf62dc71855ed8e7fecdbbb97f15
Arnold Woźnica <arni.woznica@gmail.com>
2017-11-18 10:59:41 +0100

    initial commit

---
+++ code/GeneticsAlgorithm/src/main/java/com/pwr/geneticsClassifier/GeneticsAlgorithm/App.java
@@ -0,0 +1,13 @@
+package com.pwr.geneticsClassifier.GeneticsAlgorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
